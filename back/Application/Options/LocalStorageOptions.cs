b7e87d76499caae3e7000c1258be4535c7c0c1ef
alonaarz <aser28874@gmail.com>
2026-08-21 19:52:36 +0000

    Fix dynamic local storage URL for CI/CD

--- back/Application/Options/LocalStorageOptions.cs
+++ back/Application/Options/LocalStorageOptions.cs
@@ -3,5 +3,5 @@ namespace Application.Options;
 public class LocalStorageOptions
 {
     public string RootPath { get; set; }
-    public string BaseUrl { get; set; } = "http://172.20.10.5:8080";
+    public string BaseUrl { get; set; } = string.Empty;
 }
--- back/VideoProcessor/LocalStorageOptions.cs
+++ back/VideoProcessor/LocalStorageOptions.cs
@@ -3,5 +3,5 @@ namespace VideoProcessor;
 public class LocalStorageOptions
 {
     public string RootPath { get; set; } = Path.Combine(AppContext.BaseDirectory, "storage");
-    public string BaseUrl { get; set; } = "http://172.20.10.5:8080";
+    public string BaseUrl { get; set; } = string.Empty;
 }
